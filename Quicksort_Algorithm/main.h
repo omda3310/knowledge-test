326f051f356f537aee997465aad11803fbc7c99c
omda3310 <6197@holbertonstudents.com>
2023-09-09 01:32:08 +0200

    Define functions

---
+++ Quicksort_Algorithm/main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+void swap_integers(int* x, int* y);
+int partition(int array[], int start_index, int end_index);
+void quickSort(int array[], int start_index, int end_index);
+
+#endif
